c05ddbc9c3e4a22a050696be207629617485e217
Guillaume PIRARD <guillaumepirard974@gmail.com>
2021-09-20 10:22:26 +0200

    [NEW] Mise en place de REACT-REDUX et début du menu responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,26 @@
 import './styles.scss';
+
+import { useDispatch } from 'react-redux';
+
 import logo from './images/logo.png';
 import menuClosed from './images/menu_closed.png';
 
-const Header = () => (
-  <div className="header">
-    <div className="header__menu">
-      <img src={logo} alt="logo" className="header__menu__logo" />
-      <img src={menuClosed} alt="menu_logo" className="header__menu__burger" />
+const Header = () => {
+  const dispatch = useDispatch();
+  const handleMenu = () => {
+    dispatch({
+      type: 'TOGGLE_MENU',
+    });
+  };
+
+  return (
+    <div className="header">
+      <div className="header__menu">
+        <img src={logo} alt="logo" className="header__menu__logo" />
+        <img src={menuClosed} alt="menu_logo" className="header__menu__burger" onClick={handleMenu} />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;

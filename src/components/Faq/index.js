17ae0b82b0ec8e5afde301f28bb8763f15780a20
guillaumepirard <guillaumepirard974@gmail.com>
2021-09-29 11:37:59 +0200

    [NEW] Who we are page on the FAQ

--- src/components/Faq/index.js
+++ src/components/Faq/index.js
@@ -1,8 +1,34 @@
 import './styles.scss';
+import guillaume from './images/guillaume.jpg';
 
 const Faq = () => (
   <div className="faq">
     <h1 className="faq__title">Foire aux questions</h1>
+    <div className="faq__who">
+      <p className="faq__who__p">Qui sommes nous ?</p>
+      <div className="faq__who__container">
+        <div className="faq__who__container__card">
+          <img src={guillaume} alt="" className="faq__who__container__card__img" />
+          <p className="faq__who__container__card__p">Product Owner</p>
+        </div>
+        <div className="faq__who__container__card">
+          <img src={guillaume} alt="" className="faq__who__container__card__img" />
+          <p className="faq__who__container__card__p">Lead Dev Front</p>
+        </div>
+        <div className="faq__who__container__card">
+          <img src={guillaume} alt="" className="faq__who__container__card__img" />
+          <p className="faq__who__container__card__p">Lead Dev Back</p>
+        </div>
+        <div className="faq__who__container__card">
+          <img src={guillaume} alt="" className="faq__who__container__card__img" />
+          <p className="faq__who__container__card__p">Scrum Master</p>
+        </div>
+        <div className="faq__who__container__card">
+          <img src={guillaume} alt="" className="faq__who__container__card__img" />
+          <p className="faq__who__container__card__p">Git Master</p>
+        </div>
+      </div>
+    </div>
     <div className="faq__questions">
       <h2 className="faq__questions__question">Qui est le plus BG ?</h2>
       <p className="faq__questions__response">Par élimination : pas nathan, pas amaury, pas gaelle, pas raph donc je vous laisse deviner qui c'est :)</p>
